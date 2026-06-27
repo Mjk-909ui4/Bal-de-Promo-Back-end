bfaba343c7b4ee39f249992fa1012e31aae73f65
Mjk909ui4 <manjaka208andrianomena@gmail.com>
2026-06-27 16:55:46 +0300

    maj

--- app.py
+++ app.py
@@ -32,19 +32,6 @@ def billet(id_billets):
         return "Billet utilise"
     
     else :
-        conn = sqlite3.connect('billets.db')
-        cur = conn.cursor()
-
-        cur.execute(
-            """
-            UPDATE billets
-            SET utilise = 'OUI'
-            WHERE id_billets = ?
-        """, (id_billets,)
-        )
-
-        conn.commit()
-        conn.close()
 
         return f"""
         <h2>Billet Valide</h2>
