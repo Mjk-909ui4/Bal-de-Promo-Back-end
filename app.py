6c2dcfd45440df23cc636202ce866863ba215604
Mjk909ui4 <manjaka208andrianomena@gmail.com>
2026-06-28 20:39:16 +0300

    pull

--- app.py
+++ app.py
@@ -41,16 +41,8 @@ def billet(id_billets):
         <a href="https://bal-de-promo-back-end.onrender.com/confirmation/{id_billets}">CLIENT ENTRÉE<a>
     """
 
-@app.route("/confirmation/<string:id_billets>")
-
-def valide_billets(id_billets):
-    conn = sqlite3.connect('billets.db')
-    cur = conn.cursor()
-
-    cur.execute(
-        """
-        UPDATE billets
-        SET utilise = 'OUI'
-        WHERE id_billets = ?
-    """, (id_billets,)
-    )
+@apadskjfhoiudsgfchjbask
+dfhdsgfshlkcna
+sdhafjgdfuhsbbhcjkdhfa
+ajsdhgfaoduvjbcad
+hxavisdufv
